2a9138d6fcdafc9d1c2db052e860f23b54ab190d
ThreeCoffes <leonard.madzielewski@gmail.com>
2024-08-26 01:11:04 +0200

    sqlb rewrite

--- backend/src/model/todo.rs
+++ backend/src/model/todo.rs
@@ -1,5 +1,7 @@
+use sqlb::HasFields;
+
 use super::db::Db;
-use crate::model;
+use crate::{model, security::UserCtx};
 
 #[derive(sqlx::FromRow, Debug, Clone)]
 pub struct Todo {
@@ -9,9 +11,8 @@ pub struct Todo {
     pub status: TodoStatus,
 }
 
-#[derive(Default, Debug, Clone)]
+#[derive(sqlb::Fields, Default, Debug, Clone)]
 pub struct TodoPatch {
-    pub cid: Option<i64>,
     pub title: Option<String>,
     pub status: Option<TodoStatus>,
 }
@@ -23,27 +24,32 @@ pub enum TodoStatus {
     Open,
     Close,
 }
+sqlb::bindable!(TodoStatus);
 
 pub struct TodoMac;
 
 impl TodoMac {
-    pub async fn create(db: &Db, data: TodoPatch) -> Result<Todo, model::Error> {
-        let sql = "INSERT INTO todo (cid, title) VALUES ($1, $2) returning id, cid, title, status";
-        let query = sqlx::query_as::<_, Todo>(&sql)
-            .bind(123 as i64) // FIXME - should come from user context
-            .bind(data.title.unwrap_or_else(|| "untitled".to_string()));
+    pub async fn create(db: &Db, utx: &UserCtx, data: TodoPatch) -> Result<Todo, model::Error> {
+        let mut fields = data.fields();
+        fields.push(("cid", utx.user_id).into());
 
-        let todo = query.fetch_one(db).await?;
+        let sb = sqlb::insert()
+            .table("todo")
+            .data(fields)
+            .returning(&["id", "cid", "title", "status"]);
+
+        let todo = sb.fetch_one(db).await?;
 
         Ok(todo)
     }
 
-    pub async fn list(db: &Db) -> Result<Vec<Todo>, model::Error> {
-        let sql = "SELECT id, cid, title, status FROM todo ORDER BY id DESC";
+    pub async fn list(db: &Db, utx: &UserCtx) -> Result<Vec<Todo>, model::Error> {
+        let sb = sqlb::select()
+            .table("todo")
+            .columns(&["id", "cid", "title", "status"])
+            .order_by("!id");
 
-        // build the sqlx-query
-        let query = sqlx::query_as(&sql);
-        let todos = query.fetch_all(db).await?;
+        let todos = sb.fetch_all(db).await?;
 
         Ok(todos)
     }
